cece941a563a174616f8983d6e884a1cc80ff949
gr4nny <jvspearman@gmail.com>
2014-07-24 17:52:40 -0400

    First Draft
    Some things are working, but needs many revisions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creating the inverse of a matrix is computationally expensive.
+## It would be handy to have a function that stores a matrix and its inverse.
+## This will consist of two functions.
+## 1) storing the matrix and its inverse
+## 2) comparing a matrix to the cache and returning either the matrix or its inverse
 
 ## This function opens the matrix. Once you see inside, the world will never
 ## look the same again.
+## It will evaluate if it is identical to the one in the cache and if it is not,
+## it will store it and its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
+  if(!is.matrix(x)) {                   ## check to see if x is a matrix
+    stop("x is not a matrix")
+  }
+  if(!all(x == cachedMatrix)) {         ## check to see if x is the same as the cachedMatrix
+    cachedMatrix<-x
+    inverseCachedMatrix<-cacheSolve(cachedMatrix)
+  } else {
+    print("x")
+    print("inverseCachedMatrix")
+  }
 
 }
 
 
-## Write a short comment describing this function
+## This function assumes that x is a matix, and then inverts it, 
+## returning the variable inverseCachedMatrix
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  ## check to see if the matrix is invertible
+  
+  ## invert the matrix
+  inversedMatrix<<-solve(x)
 }
